b8c4f4e007d1814b64c920c26c68762c052edc18
Harshit Nayan <harshitrajeevnayan@gmail.com>
2026-09-10 04:15:55 +0530

    mel spectrogram

--- src/dataset.py
+++ src/dataset.py
@@ -1,25 +1,27 @@
 import pandas as pd
 import torch
 import torch.nn.functional as F
-import torchaudio
 from torch.utils.data import Dataset
 import librosa
 from tokenizer import tokenizer
+import matplotlib.pyplot as plt
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
 def load_wav(path_to_audio, sr = 22050):
 
-    audio, orig_sr = torchaudio.load(path_to_audio)
+    audio, orig_sr = librosa.load(path_to_audio)
 
     if sr != orig_sr:
-        audio = torchaudio.functional.resample(audio, orig_freq = orig_sr, new_freq = sr)
+        audio = librosa.resample(audio, orig_freq = orig_sr, new_freq = sr)
 
-    return audio.squeeze(0)
+    return audio
 # converts to decible 
 def amp_to_db(x, min_db = -100):
-    #Formula = 20*torch.log10(1e-5) = 20* -5 = -100
-    clip_val = 10** (min_db/20)
-    return 20 * torch.log10(torch.clamp(x, min=min_db))
+     #Formula = 20*torch.log10(1e-5) = 20* -5 = -100
+    clip_val = 10 ** (min_db / 20)
+    return 20 * torch.log10(torch.clamp(x, min=clip_val))
 
 def db_to_amp(x):
     return 10**(x/20)
@@ -77,6 +79,40 @@ class AudioMelConversion:
 
         return torch.from_numpy(mel)
 
+    def audio2mel(self, audio_ , do_norm = False):
+
+        if not isinstance(audio_, torch.Tensor):
+            audio_ = torch.tensor(audio_, dtype = torch.float32)
+
+        spectrogram = torch.stft(audio_,
+                               n_fft = self.n_fft,
+                               hop_length = self.window_size,
+                               win_length = self.window_size,
+                               window = torch.hann_window(self.window_size),
+                               center = self.center,
+                               pad_mode = "reflect",
+                               normalized = False,
+                                onesided = True,
+                               return_complex = True)
+
+        spectrogram = torch.abs(spectrogram)
+
+        mel = torch.matmul(self.spec2mel.to(spectrogram.device), spectrogram)
+        mel = amp_to_db(mel, self.min_db)
+
+        if do_norm:
+            mel = normalize(mel, min_db = self.min_db, max_abs_val= self.max_scaled_abs)
+
+        return (mel)
+
+
 if __name__ == "__main__":
+    path_to_audio = r"D:\TTS\data\LJSpeech-1.1\wavs\LJ034-0199.wav"
+    audio = load_wav(path_to_audio)
+    print(audio)
+
     amc = AudioMelConversion()
+    mel = amc.audio2mel(audio, do_norm=True)
+    print(mel)
+    
 
