99b4c48865408d7f59380beb492345cfd924639e
Harshit Nayan <harshitrajeevnayan@gmail.com>
2026-09-14 15:05:52 +0530

    character encoder

--- src/model.py
+++ src/model.py
@@ -91,3 +91,59 @@ class ConvNorm(nn.Module):
     def forward (self, x):
         return self.conv(x)
 
+class Encoder(nn.Module):
+    def __init__(self,config):
+        super(Encoder, self).__init__()
+
+        self.config = config
+        self. embeddings = nn.Embedding(config.num_chars, config.character_embed_dim, padding_idx=config.pad_token_id)
+        self.convulations = nn.ModuleList()
+
+        for i in range(config.encoder_n_convulations):
+
+            self.convulations.append(
+                nn.Sequential(
+                    ConvNorm(
+                        in_channels = config.encoder_embed_dim if i != 0 else config.character_embed_dim,
+                        out_channels= config.encoder_embed_dim,
+                        kernel_size= config.encoder_kernel_size,
+                        stride = 1,
+                        padding = "same",
+                        dilation= 1,
+                        w_init_grain="relu"
+
+                    ),
+
+                    nn.BatchNormld(config.encoder_embed_dim),
+                    nn.ReLU(),
+                    nn.Dropout(config.enocder_dropout_p)
+                )
+            )
+        self .lstm = nn.LSTM(
+            input_size = config.encoder_embed_dim,
+            hidden_size= config.encoder_embed_dim//2,
+            num_layers= 1,
+            batch_first = True,
+            bidirectional= True
+        )
+
+    def forward(self, x , input_lenghts = None):
+
+        x = self.embeddings(x).transpose(1,2)
+
+        batch_size, channels, seq_len = x.shape
+
+        if input_lenghts is None:
+            input_lenghts = torch.full((batch_size,), fill_value= seq_len, device = x.device)
+
+        for block in self.blocks:
+            x = block(x)
+
+        x = x.transpose(1,2)
+        x = pack_padded_sequence(x, input_lenghts.cpu(), batch_first= True)
+
+        outputs, _= self.lstm(x)
+        outputs, _= pad_packed_sequence(outputs, batch_first= True)
+
+        return outputs
+
